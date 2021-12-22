7624b6947923cb12fa2718d5187cda889dc0c209
karlobratko <karlobratko@gmail.com>
2021-12-23 00:14:44 +0100

    Initial commit

---
+++ Sandbox/src/Source.cpp
@@ -0,0 +1,11 @@
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+
+constexpr const char* WINDOW_NAME		= "LearnOpenGL";
+constexpr int					WINDOW_WIDTH	= 800;
+constexpr int					WINDOW_HEIGHT = 600;
+
+int main() {
+
+	return 0;
+}
