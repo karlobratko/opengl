bfffe24e5aa7af3aaf2ae85aa9ec0dd871d357b1
Karlo Bratko <47771011+karlobratko@users.noreply.github.com>
2022-01-06 01:28:21 +0100

    Deleted TestingEnvironment directory

--- TestingEnvironment/src/IBindable.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-class IBindable {
-public:
-	virtual ~IBindable() = 0;
-	virtual void Bind() const = 0;
-	virtual void UnBind() const = 0;
-};
--- TestingEnvironment/src/IDisposable.cpp
+++
@@ -1,3 +0,0 @@
-#include "IDisposable.h"
-
-IDisposable::~IDisposable() {}
